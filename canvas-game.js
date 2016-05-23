9d1efeadda45d152b7fcc6bb890273547c4dde3f
Carolyn Daniel <carolyn.s.daniel@gmail.com>
2016-05-23 10:30:14 -0400

    add monster to board

--- canvas-game.js
+++ canvas-game.js
@@ -8,9 +8,15 @@ var heroX = 200;
 var heroY = 200;
 var heroSpeed = 5;
 
+//add monster to board
+var monsterImage = new Image();
+monsterImage.src = 'images/monster.png';
+var monsterX = 300;
+var monsterY = 300;
+var monsterSpeed = 5;
+
 window.addEventListener('keydown', function(event) {
   var key = event.keyCode;
-  console.log(key);
   if (key === 37) { //left
     heroX -= heroSpeed;
   } else if (key === 39) { //right
@@ -20,11 +26,28 @@ window.addEventListener('keydown', function(event) {
   } else if (key === 40) {
     heroY += heroSpeed;
   }
+
+  if (heroX > 512) {
+    heroX = 0;
+  }
+  if (heroX < 0) {
+    heroX = 512;
+  }
+  if (heroY > 480) {
+    heroY = 0;
+  }
+  if (heroY < 0) {
+    heroY = 480;
+  }
+
+  console.log('heroX: ' + heroX);
 });
 
 function main () {
   ctx.drawImage(backgroundImage, 0, 0);
   ctx.drawImage(heroImage, heroX, heroY);
+  monsterX += monsterSpeed;
+  ctx.drawImage(monsterImage, monsterX, monsterY);
   requestAnimationFrame(main);
 }
 main();
